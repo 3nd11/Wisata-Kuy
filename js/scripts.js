19ea7e73bd7fe654b40deb536dd56358077d3656
3nd11 <76111424+3nd11@users.noreply.github.com>
2021-01-21 01:36:04 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+var attempt = 3;
+
+function validasi() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "endi12" && password == "endi12") {
+        alert("It's Correct, Welcome Bro!")
+        window.open('../src/admin/Home Admin.html','_self', false);
+    }
+    else if (username == "" && password == ""){
+        alert("Who Are You?!")
+        return false
+    }
+}
+
+function fungsi1() {
+    var myobj = document.getElementById("1");
+    myobj.remove();
+}
+function fungsi2() {
+    var myobj = document.getElementById("2");
+    myobj.remove();
+}
+function fungsi3() {
+    var myobj = document.getElementById("3");
+    myobj.remove();
+}
+function fungsi4() {
+    var myobj = document.getElementById("4");
+    myobj.remove();
+}
+function fungsi5() {
+    var myobj = document.getElementById("5");
+    myobj.remove();
+}
+function fungsi6() {
+    var myobj = document.getElementById("6");
+    myobj.remove();
+}
+function fungsi7() {
+    var myobj = document.getElementById("7");
+    myobj.remove();
+}
+function fungsi8() {
+    var myobj = document.getElementById("8");
+    myobj.remove();
+}
+function fungsi9() {
+    var myobj = document.getElementById("9");
+    myobj.remove();
+}
+function fungsi10() {
+    var myobj = document.getElementById("10");
+    myobj.remove();
+}
+function fungsi11() {
+    var myobj = document.getElementById("11");
+    myobj.remove();
+}
+function fungsi12() {
+    var myobj = document.getElementById("12");
+    myobj.remove();
+}
+function fungsi13() {
+    var myobj = document.getElementById("13");
+    myobj.remove();
+}
+function fungs114() {
+    var myobj = document.getElementById("14");
+    myobj.remove();
+}
+function fungsi15() {
+    var myobj = document.getElementById("15");
+    myobj.remove();
+}
+function fungsi16() {
+    var myobj = document.getElementById("16");
+    myobj.remove();
+}
+function fungsi17() {
+    var myobj = document.getElementById("17");
+    myobj.remove();
+}
+function fungsi18() {
+    var myobj = document.getElementById("18");
+    myobj.remove();
+}
